8d0e1bc1f11e8e4cf4f46c250f5b4a55990c5fb0
happyjerryceo <coro0057@gmail.com>
2021-07-15 21:51:48 +0900

    project (2)

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crudapp.apps.CrudappConfig',
+    'crudmember.apps.CrudmemberConfig',
 ]
 
 MIDDLEWARE = [
